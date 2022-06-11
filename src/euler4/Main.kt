4327249502867c5ffe88b295de0e357251f1f7a5
Danylo Hrebeniuk <daniilgrebenuk2002@gmail.com>
2022-06-11 12:02:03 +0200

    Problem 26

--- src/euler4/Main.kt
+++ src/euler4/Main.kt
@@ -3,12 +3,13 @@ package euler4
 fun main() {
     var res = 0
     for (i in 100..999){
-        for (j in i..999){
+        for (j in 999 downTo i){
             val temp = i * j
-            if (temp > res) {
-                if (temp.toString() == temp.toString().reversed()) {
-                    res = temp
-                }
+            if (temp <= res){
+                break
+            }
+            if (temp.toString() == temp.toString().reversed()) {
+                res = temp
             }
         }
     }
