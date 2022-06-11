ce13bd7ecad6fd22ce834033d8753621229f8adc
Danylo Hrebeniuk <daniilgrebenuk2002@gmail.com>
2022-06-11 14:28:25 +0200

    Problem 16

---
+++ src/euler16/Main.kt
@@ -0,0 +1,15 @@
+package euler16
+
+import java.math.BigInteger
+
+fun main() {
+    val power = 1000
+
+    println(
+        BigInteger.valueOf(2L).pow(power)
+            .toString()
+            .chars()
+            .map { it - 48}
+            .sum()
+    )
+}
