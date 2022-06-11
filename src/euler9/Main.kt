52f31d55827fcb4867ea45a0396e09052d7f7c9d
Danylo Hrebeniuk <daniilgrebenuk2002@gmail.com>
2022-06-11 13:19:49 +0200

    Problem 9

---
+++ src/euler9/Main.kt
@@ -0,0 +1,16 @@
+package euler9
+
+import kotlin.math.pow
+
+fun main() {
+    val s = 1000L
+
+    for (a in 3..s/3) {
+        for (b in a..s/2) {
+            if (a*a + b*b == (s - a - b).toDouble().pow(2).toLong()){
+                println(a*b*(s-a-b))
+                return
+            }
+        }
+    }
+}
