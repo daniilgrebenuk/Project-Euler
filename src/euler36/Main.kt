0f6aa7e59d820f1cdc8bf0098e83982c8ebf91ae
Danylo Hrebeniuk <daniilgrebenuk2002@gmail.com>
2022-06-13 15:54:04 +0200

    Problem 36

---
+++ src/euler36/Main.kt
@@ -0,0 +1,22 @@
+package euler36
+
+fun main() {
+    var sum = 0
+    for (i in 1..1_000_000) {
+        if (i.isFullPalindromic())
+            sum += i
+    }
+    println(sum)
+}
+
+fun Int.isFullPalindromic(): Boolean = isPalindromic(this.toString()) && isPalindromic(this.toString(2))
+
+fun isPalindromic(s: String): Boolean {
+    val chars = s.toCharArray()
+    for (i in 0..chars.size / 2) {
+        if (chars[i] != chars[chars.size - i - 1])
+            return false
+    }
+    return true
+}
+
