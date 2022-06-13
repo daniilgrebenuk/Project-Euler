de64534a201ae5e2acf90b056685c48550233444
Danylo Hrebeniuk <daniilgrebenuk2002@gmail.com>
2022-06-13 16:05:46 +0200

    Problem 37

---
+++ src/euler37/Main.kt
@@ -0,0 +1,44 @@
+package euler37
+
+import kotlin.math.sqrt
+
+fun main() {
+    val primes = getPrimesBelowNumber(1_000_000)
+
+    fun Int.isInterestingPrime(): Boolean {
+        if(this < 10)
+            return false
+
+        val primeString = this.toString()
+        for (i in 1 until primeString.length) {
+            if (!(primes.contains(primeString.substring(i).toInt()) &&
+                        primes.contains(primeString.substring(0, primeString.length - i).toInt()))
+            ) return false
+        }
+        return true
+    }
+
+    var sum = 0
+    for (prime in primes) {
+        if (prime.isInterestingPrime())
+            sum += prime
+    }
+    println(sum)
+}
+
+
+fun getPrimesBelowNumber(number: Int): Set<Int> {
+    val primes = HashSet<Int>()
+    val booleanArray = BooleanArray(number) { true }
+    booleanArray[0] = false
+    booleanArray[1] = false
+    for (i in 2..sqrt(number.toDouble()).toInt()) {
+        if (!booleanArray[i])
+            continue
+        for (j in i * i until number step i) {
+            booleanArray[j] = false
+        }
+    }
+    booleanArray.forEachIndexed { index, b -> if (b) primes += index }
+    return primes
+}
