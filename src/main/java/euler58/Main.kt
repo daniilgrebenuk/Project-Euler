2744ecd670ac41e47475b3c5e29f291302b2efe4
Danylo Hrebeniuk <daniilgrebenuk2002@gmail.com>
2022-07-30 08:34:24 +0200

    Problem 58

---
+++ src/main/java/euler58/Main.kt
@@ -0,0 +1,60 @@
+package euler58
+
+import java.math.BigInteger
+
+/*
+import kotlin.math.sqrt
+
+fun main() {
+    var primeCounter = .0
+    var notPrimeCounter = 1.0
+
+    var spiralCounter = 0
+    var next = 2
+    var adder = 2
+    while (true) {
+        if (spiralCounter == 0) {
+            next += adder - 1
+        } else if (spiralCounter == 4) {
+            next++
+            spiralCounter = 0
+            adder += 2
+
+            if (primeCounter / (primeCounter + notPrimeCounter) < .10) {
+                println(adder - 1)
+                break
+            }
+            continue
+        } else {
+            next += adder
+        }
+        spiralCounter++
+        if ((next).isPrime()){
+            primeCounter++
+        }else {
+            notPrimeCounter++
+        }
+    }
+
+
+}
+
+private fun Int.isPrime():Boolean {
+    for (i in 2 .. sqrt(this.toDouble()).toInt()) {
+        if (this % i == 0)
+            return false
+    }
+    return true
+}*/
+
+fun main() {
+    println(100.factorial())
+}
+
+fun Int.factorial(): BigInteger {
+    var temp = BigInteger.ONE
+    for (i in 1..this){
+        temp *= i.toBigInteger()
+    }
+    return temp
+}
