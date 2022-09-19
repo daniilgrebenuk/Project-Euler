3e996d32226b48d2136318e73eb3b9140d753383
Danylo Hrebeniuk <daniilgrebenuk2002@gmail.com>
2022-09-19 16:49:36 +0200

    Problem 70

---
+++ src/main/java/euler70/Main.kt
@@ -0,0 +1,59 @@
+package euler70
+
+import kotlin.math.sqrt
+
+fun main() {
+    val start = System.currentTimeMillis()
+    val primes = getPrimesBelowNumber(4_000)
+
+    val numbers = ArrayList<Pair<Int, List<Int>>>()
+
+
+    outer@ for (i in primes.indices) {
+        for (j in (i + 1) until primes.size) {
+            val temp = primes[i] * primes[j]
+            if (temp < 10_000_000)
+                numbers += temp to listOf(primes[i], primes[j])
+            else
+                continue@outer
+        }
+    }
+    var lowest = 0 to 1.1
+    for (i in numbers) {
+        val resultFi = fiFunction(i)
+        val temp = i.first.toDouble() / resultFi
+        if (isPermutation(i.first, resultFi) && temp < lowest.second) {
+            lowest = i.first to temp
+        }
+    }
+    println(lowest.first)
+    println("Time: ${System.currentTimeMillis() - start}")
+}
+
+fun isPermutation(firstNumber: Int, secondNumber: Int) =
+    firstNumber.toString().toCharArray().sorted() == secondNumber.toString().toCharArray().sorted()
+
+fun fiFunction(number: Pair<Int, List<Int>>): Int {
+    var res = number.first + 1
+    for (div in number.second) {
+        res -= div
+    }
+
+    return res
+}
+
+private fun getPrimesBelowNumber(number: Int): List<Int> {
+    val primes = ArrayList<Int>()
+    val booleanArray = BooleanArray(number) { true }
+    booleanArray[0] = false
+    booleanArray[1] = false
+    for (i in 2..sqrt(number.toDouble()).toInt()) {
+        if (!booleanArray[i])
+            continue
+        for (j in i * i until number step i) {
+            booleanArray[j] = false
+        }
+    }
+    booleanArray.forEachIndexed { index, b -> if (b) primes += index }
+    return primes
+}
